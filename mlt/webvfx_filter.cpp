563a04791aea64d9c3e088cb8d237a4b68d2bc32
Dan Dennedy <dan@dennedy.org>
2019-05-26 20:27:16 -0700

    Add transparent property to MLT filter.
    When set 1, this gives a filter a transparent canvas for drawing with the
    source image still available through the JavaScript extension.

--- mlt/webvfx_filter.cpp
+++ mlt/webvfx_filter.cpp
@@ -9,6 +9,7 @@ extern "C" {
     #include <mlt/framework/mlt_consumer.h>
 }
 #include <webvfx/image.h>
+#include <memory>
 #include "factory.h"
 #include "service_locker.h"
 #include "service_manager.h"
@@ -34,10 +35,31 @@ static int filterGetImage(mlt_frame frame, uint8_t **image, mlt_image_format *fo
             return 1;
 
         bool hasAlpha = (*format == mlt_image_rgb24a);
-        int size = *width * *height * (hasAlpha? 4 : 3);
-        WebVfx::Image renderedImage(*image, *width, *height, size, hasAlpha);
+        std::unique_ptr<WebVfx::Image> sourceImage;
+        std::unique_ptr<WebVfx::Image> renderedImage;
+        uint8_t* buffer = nullptr;
+        
+        if (mlt_properties_get_int(MLT_FILTER_PROPERTIES(filter), "transparent")) {
+            int size = mlt_image_format_size(*format, *width, *height, NULL);
+            // Create a new buffer for the source image.
+            buffer = (uint8_t*) mlt_pool_alloc(size);
+            memcpy(buffer, *image, size);
+            // Make the background white.
+            memset(*image, 255  , size);
+            size = *width * *height * (hasAlpha? 4 : 3);
+            // Make the background transparent.
+            for (int i = 0; i < *width * *height; i++)
+                (*image)[4 * i + 3] = 0;
+            sourceImage.reset(new WebVfx::Image(buffer, *width, *height, size, hasAlpha));
+            renderedImage.reset(new WebVfx::Image(*image, *width, *height, size, hasAlpha));
+        } else {
+            int size = *width * *height * (hasAlpha? 4 : 3);
+            sourceImage.reset(new WebVfx::Image(*image, *width, *height, size, hasAlpha));
+            renderedImage.reset(new WebVfx::Image(*image, *width, *height, size, hasAlpha));
+        }
+
         MLTWebVfx::ServiceManager* manager = locker.getManager();
-        manager->setImageForName(manager->getSourceImageName(), &renderedImage);
+        manager->setImageForName(manager->getSourceImageName(), sourceImage.get());
         manager->setupConsumerListener(frame);
 
         // If there is a consumer set on the frame and the consumer is stopped,
@@ -47,7 +69,8 @@ static int filterGetImage(mlt_frame frame, uint8_t **image, mlt_image_format *fo
         mlt_consumer consumer = static_cast<mlt_consumer>(
             mlt_properties_get_data(MLT_FRAME_PROPERTIES(frame), "consumer", NULL));
         if (!consumer || !mlt_consumer_is_stopped(consumer))
-            manager->render(&renderedImage, position, length, hasAlpha);
+            manager->render(renderedImage.get(), position, length, hasAlpha);
+        mlt_pool_release(buffer);
     }
 
     return error;
